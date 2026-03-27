31ce19eda641e6d22492729b02980bce3b265906
Huy24vt <130732635+Huy24vt@users.noreply.github.com>
2026-03-27 16:19:12 +0700

    Simplify run_step function by removing return

--- src/update_pipeline.py
+++ src/update_pipeline.py
@@ -1,14 +1,16 @@
 import subprocess
 import sys
 
+
 def run_step(script_path: str):
-    result = subprocess.run([sys.executable, script_path], check=True, text=True)
-    return result
+    subprocess.run([sys.executable, script_path], check=True)
+
 
 def main():
     run_step("src/fetch_price.py")
     run_step("src/process_price.py")
     print("Update completed.")
+
 
 if __name__ == "__main__":
     main()
