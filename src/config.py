080f7aa6f4e567a0d428c5c9ee08ad990ebd5326
Huy24vt <130732635+Huy24vt@users.noreply.github.com>
2026-03-27 17:03:10 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,7 +1,2 @@
-VN30_SYMBOLS = [
-    "ACB", "BCM", "BID", "BVH", "CTG", "FPT", "GAS", "GVR", "HDB", "HPG",
-    "MBB", "MSN", "MWG", "PLX", "POW", "SAB", "SHB", "SSB", "SSI", "STB",
-    "TCB", "TPB", "VCB", "VHM", "VIB", "VIC", "VJC", "VNM", "VPB", "VRE"
-]
-
+SYMBOLS = ["ACB", "FPT", "HPG"]
 START_DATE = "2024-01-01"
