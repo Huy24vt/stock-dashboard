9ea18fe406932e7b5fc2261894aa65197ff1a59f
Huy24vt <130732635+Huy24vt@users.noreply.github.com>
2026-03-27 16:17:28 +0700

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,7 @@
+VN30_SYMBOLS = [
+    "ACB", "BCM", "BID", "BVH", "CTG", "FPT", "GAS", "GVR", "HDB", "HPG",
+    "MBB", "MSN", "MWG", "PLX", "POW", "SAB", "SHB", "SSB", "SSI", "STB",
+    "TCB", "TPB", "VCB", "VHM", "VIB", "VIC", "VJC", "VNM", "VPB", "VRE"
+]
+
+START_DATE = "2024-01-01"
